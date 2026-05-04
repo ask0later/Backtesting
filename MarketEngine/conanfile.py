02711c8f8143f6fba6a61c63b9e85ed1ead60cff
Yaroslav Kuznetsov <asklater@bk.ru>
2026-05-04 16:27:51 +0300

    [build] Initialize CMake and Conan configuration

---
+++ MarketEngine/conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
+
+class MarketEngineConan(ConanFile):
+    name = "MarketEngine"
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("gtest/1.17.0")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.27]")
+
+    def layout(self):
+        cmake_layout(self)
